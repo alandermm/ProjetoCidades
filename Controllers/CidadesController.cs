f9bc23d5951444243a9df5cd8ea739c1a5155128
Alander Machado <alandermm@hotmail.com>
2018-01-12 19:03:36 -0200

    Utilização do Banco de Dados

--- Controllers/CidadesController.cs
+++ Controllers/CidadesController.cs
@@ -1,26 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjetoCidades.Models;
+using ProjetoCidades.Repositorio;
 
 namespace ProjetoCidades.Controllers
 {
     public class CidadesController:Controller
     {
         Cidade cidade = new Cidade();
+        CidadeRep objCidadeRep = new CidadeRep();
         public IActionResult Index(){
-            var lista = cidade.ListarCidades();
+            var lista = objCidadeRep.Listar();
             return View(lista);
         }
 
         public IActionResult CidadesEstados(){
-            var lista = cidade.ListarCidades();
+            var lista = objCidadeRep.Listar();
             return View(lista);
         }
 
         public IActionResult TodosOsDados(){
-            var lista = cidade.ListarCidades();
+            var lista = objCidadeRep.Listar();
             return View(lista);
         }
+        
+        [HttpGet]
+        public IActionResult Cadastrar(){
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult Cadastrar([Bind] Cidade cidade){
+            objCidadeRep.Cadastrar(cidade);
+            return RedirectToAction("Index");
+        }
 
     }
 }
---
+++ Repositorio/CidadeRep.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using ProjetoCidades.Models;
+
+namespace ProjetoCidades.Repositorio
+{
+    public class CidadeRep
+    {
+        string connectionString = @"Data source=.\sqlExpress;Initial Catalog=ProjetoCidades;uid=sa;pwd=senai@123";
+
+        public List<Cidade> Listar(){
+            List<Cidade> lstCidades = new List<Cidade>();
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string SqlQuery = "Select * from Cidades";
+
+            SqlCommand cmd = new SqlCommand(SqlQuery, con);
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                Cidade cidade = new Cidade();
+
+                cidade.Id = Convert.ToInt16(sdr["Id"]);
+                cidade.Nome = sdr["Nome"].ToString();
+                cidade.Estado = sdr["Estado"].ToString();
+                cidade.Habitantes = Convert.ToInt32(sdr["Habitantes"]);
+
+                lstCidades.Add(cidade);
+            }
+
+            con.Close();
+
+            return lstCidades;
+        }
+
+        public void Cadastrar(Cidade cidade){
+            SqlConnection con = new SqlConnection(connectionString);
+            
+            string SqlQuery = "Insert into Cidades (Nome, Estado, Habitantes) values ('" + cidade.Nome + "', '" + cidade.Estado + "', '" + cidade.Habitantes + "')";
+
+            SqlCommand cmd = new SqlCommand(SqlQuery, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+    }
+}
